269bce2d278581c405aac663e74ffccff9e96bac
Aurelio Malheiros <aurelio.malheiros@unico.io>
2022-07-23 07:22:29 -0300

    feat: lambda

---
+++ problems/advanced/lambda/lambda.py
@@ -0,0 +1,13 @@
+"""
+Function lambda
+"""
+list = [
+    ['value1', 5],
+    ['value3', 11],
+    ['value2', 100],
+    ['value4', 8]
+]
+print("Product name")
+print(sorted(list, key=lambda i: i[0], reverse=True))
+print("Product key")
+print(sorted(list, key=lambda i: i[1], reverse=True))
